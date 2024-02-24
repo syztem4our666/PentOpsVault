d1d1a580ca49c96306e61071efe7fab30af9bf4b
syztem4our666 <SYSTEMFOUR666@proton.me>
2024-02-24 15:49:22 +0100

    fixed analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import './global.css';
 import { RootProvider } from 'fumadocs-ui/provider';
 import { Inter } from 'next/font/google';
 import type { ReactNode } from 'react';
-import { Analytics } from "@vercel/analytics/react"
+import { Analytics } from '@vercel/analytics/react';
 
 const inter = Inter({
   subsets: ['latin'],
@@ -13,6 +13,7 @@ export default function Layout({ children }: { children: ReactNode }) {
     <html lang="en" className={inter.className}>
       <body>
         <RootProvider>{children}</RootProvider>
+        <Analytics />
       </body>
     </html>
   );
