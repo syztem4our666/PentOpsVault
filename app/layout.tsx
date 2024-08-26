dc5723e729df6cc44be16b85656f823dfdcc4dcf
syztem4our666 <141962485+syztem4our666@users.noreply.github.com>
2024-08-26 13:25:21 +0200

    backup

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,6 @@ export default function Layout({ children }: { children: ReactNode }) {
     <html lang="en" className={inter.className} suppressHydrationWarning>
       <body>
         <RootProvider>{children}</RootProvider>
-        <Analytics />
       </body>
     </html>
   );
